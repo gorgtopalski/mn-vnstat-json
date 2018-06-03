13bca193ea80233017de3aafddeb809b501de3f9
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2018-06-03 11:00:41 +0200

    misc: add some comments

--- src/main/groovy/io/topalski/utils/VnstatWrapper.groovy
+++ src/main/groovy/io/topalski/utils/VnstatWrapper.groovy
@@ -9,18 +9,22 @@ class VnstatWrapper
 
     static getAll()
     {
+        //Execute and retrieve vnstat information
         "vnstat --json".execute().text
     }
 
     static HttpResponse getInterface(String iface)
     {
+        //Default not found response
         def response = HttpResponse.notFound().body(notFound(iface))
 
+        //Get a list of all available network interfaces
         def list = "ls /sys/class/net".execute().text.readLines()
         if (list.contains(iface))
         {
             def txt = "vnstat -i $iface --json".execute().text
 
+            //Ensure that the interface is monitored by vnstat
             if (!txt.contains('Error'))
                 response = HttpResponse.ok().body(txt)
         }
